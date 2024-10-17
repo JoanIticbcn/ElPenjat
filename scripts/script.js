5a3b9282ba1ca104c24f79d6218c5ee9bfdb6aa8
Joan_Montanya <2023_joan.montanya@iticbnc.cat>
2024-10-17 17:26:29 +0200

    Multijugador

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@ let paraulaSecretaArray = [];
 let paraulaSecretaArrayDisplay = [];
 //Declarem les variables globals necessaries que utilitzrem més endavant
 let paraulah2 = document.getElementById("paraula-display")
-let puntsPactual = 0;
+//Variable de punts de la partida P1 i P2
+let puntsPactual = [0, 0];
 let totalPartidas = 0;
 let partidasGuanyades = 0;
 let winStreak = 1;
@@ -13,6 +14,9 @@ let valorDeLaLletra;
 let nJugades = 10;
 let imatgepenjat = document.getElementById("imgPenjat");
 let imgNumJugades = 0;
+//Creem les variables per al multijugador
+let torn = 1
+let partidesGuanyades2 = 0;
 //Desactivem les lletres i "COMENCEM"
 desactivarLletres()
 paraulah2.textContent = "COMENCEM"
@@ -35,28 +39,36 @@ function començem() {
                 document.getElementById("btn-comencem").disabled = true;
                 document.getElementById("eye").disabled = true;
                 //Netejem les arrays per començar
-                for(h=0;h<paraulaSecretaArray.length;h++){
+                for (h = 0; h < paraulaSecretaArray.length; h++) {
                     paraulaSecretaArray.pop()
                 }
-                for(j=0;j<paraulaSecretaArrayDisplay.length;j++){
+                for (j = 0; j < paraulaSecretaArrayDisplay.length; j++) {
                     paraulaSecretaArrayDisplay.pop()
                 }
                 //Creem la Array amb la paraula amb el replace si te espai i si no amb el Array.from
                 if (paraulaSecreta.indexOf(" ") > -1) {
-                    paraulaSecretaArray = paraulaSecreta.replaceAll(" ","")
+                    paraulaSecretaArray = paraulaSecreta.replaceAll(" ", "")
                     console.log(paraulaSecretaArray)
                 } else {
                     paraulaSecretaArray = Array.from(paraulaSecreta)
                 }
                 //Posem els valors inicials a les variables i el text corresponent al HTML
+                document.getElementById("player" + torn).style.backgroundColor = "green"
                 nJugades = 10;
-                puntsPactual = 0
+                puntsPactual[0] = 0
+                puntsPactual[1] = 0
                 winStreak = 1;
                 imgNumJugades = 0
+                //Player 1
                 document.getElementById("njugadas").textContent = nJugades
-                document.getElementById("ppactual").textContent = puntsPactual
+                document.getElementById("ppactual").textContent = puntsPactual[0]
                 document.getElementById("totalPartidas").textContent = totalPartidas
                 document.getElementById("partidesGuanyades").textContent = partidasGuanyades
+                //Player 2
+                document.getElementById("njugadas2").textContent = nJugades
+                document.getElementById("ppactual2").textContent = puntsPactual[1]
+                document.getElementById("totalPartidas2").textContent = totalPartidas
+                document.getElementById("partidesGuanyades2").textContent = partidesGuanyades2
                 imatgepenjat.src = "imgs/penjat_" + imgNumJugades + ".jpg"
                 //Posem els guions - a la paraula del mitg
                 let paraulaaux = ""
@@ -102,7 +114,11 @@ function comprovacions() {
             //Si la lletra coincideix la posem a la array de Display
             if (paraulaSecretaArray[index] == valorDeLaLletra) {
                 paraulaSecretaArrayDisplay[index] = valorDeLaLletra
-                puntsPactual += 1 * winStreak
+                if (torn == 1) {
+                    puntsPactual[0] += 1 * winStreak
+                } else {
+                    puntsPactual[1] += 1 * winStreak
+                }
                 winStreak += 1
             }
         }
@@ -114,21 +130,38 @@ function comprovacions() {
         }
         //Actualitzem el HTML
         paraulah2.textContent = auxiliarP.toLocaleUpperCase()
-        document.getElementById("ppactual").textContent = puntsPactual 
+        document.getElementById("ppactual").textContent = puntsPactual[0]
+        document.getElementById("ppactual2").textContent = puntsPactual[1]
     } else {
         //Si la lletra premuda no es acertada restem els punts la jugada i resetejem el winstreak, actualitzem els valors corresponents al HTML
         nJugades = nJugades - 1;
         winStreak = 1
         document.getElementById("njugadas").textContent = nJugades
+        document.getElementById("njugadas2").textContent = nJugades
         //Per a prevenir punts negatius
-        if (puntsPactual > 0) {
-            puntsPactual -= 1
-            document.getElementById("ppactual").textContent = puntsPactual
+        if (puntsPactual[0] > 0 && puntsPactual[1] > 0) {
+            if (torn == 1) {
+                puntsPactual[0] -= 1
+                document.getElementById("ppactual").textContent = puntsPactual[0]
+            } else {
+                puntsPactual[1] -= 1
+                document.getElementById("ppactual2").textContent = puntsPactual[1]
+            }
         }
         //Per a canviar la imatge indicadora del penjat
-        if (imgNumJugades <=10) {
+        if (imgNumJugades <= 10) {
             imgNumJugades += 1
             imatgepenjat.src = "imgs/penjat_" + imgNumJugades + ".jpg"
+        }
+        //Actualitzem el torn del jugador i canviem el color de les tarjetes
+        if (torn == 1) {
+            torn = 2
+            document.getElementById("player2").style.backgroundColor = "green"
+            document.getElementById("player1").style.backgroundColor = "red"
+        } else {
+            torn = 1
+            document.getElementById("player1").style.backgroundColor = "green"
+            document.getElementById("player2").style.backgroundColor = "red"
         }
     }
     //IF que comprova si hem guanyat quan auxiliarP2 === paraulaSecreta
@@ -141,17 +174,48 @@ function comprovacions() {
         //Reinicialitzem nJugades per evitar conflictes i actualitzem el recompte de partides i ho posem tot al HTML
         nJugades = 10;
         totalPartidas += 1
-        partidasGuanyades += 1
+        //Sumem les partides guanyades a qui hagi guanyat
+        if(puntsPactual[0]==puntsPactual[1]){
+            alert("Hi ha hagut un empat")
+        }else{
+            if(puntsPactual[0]>puntsPactual[1]){
+                partidasGuanyades+=1
+            }else{
+                partidesGuanyades2+=1
+            }
+        }
+
         document.getElementById("totalPartidas").textContent = totalPartidas
-        document.getElementById("partidesGuanyades").textContent = partidasGuanyades
+        document.getElementById("totalPartidas2").textContent = totalPartidas
+        if (torn == 1) {
+            document.getElementById("partidesGuanyades").textContent = partidasGuanyades
+        } else {
+            document.getElementById("partidesGuanyades2").textContent = partidesGuanyades2
+        }
+
         //Comprovem la maxima puntuació i formatejem el text amb el Dia Mes i Punts Mes grans
-        if (puntsPactual > maximPunts) {
-            maximPunts = puntsPactual
+        if (puntsPactual[0] > maximPunts) {
+            maximPunts = puntsPactual[0]
         }
         let dataActual = new Date()
         document.getElementById("partidaMesAlta").textContent = "Dia " + dataActual.getDate() + " Mes " + (dataActual.getMonth() + 1) + " Punts " + maximPunts
+        //Player 2
+        if (puntsPactual[1] > maximPunts) {
+            maximPunts = puntsPactual[1]
+        }
+        document.getElementById("partidaMesAlta2").textContent = "Dia " + dataActual.getDate() + " Mes " + (dataActual.getMonth() + 1) + " Punts " + maximPunts
         //Desacivem les lletres
         desactivarLletres()
+        //Determinem el guanyador de la partida al final o si hi ha hagut un empat
+        if(puntsPactual[0]==puntsPactual[1]){
+            alert("Hi ha hagut un empat")
+        }else{
+            if(puntsPactual[0]>puntsPactual[1]){
+                alert("Guanya el jugador 1")
+            }else{
+                alert("Guanya el jugador 2")
+            }
+        }
     }
     //IF que comprova si hem perdut o sigui nJugades <= 0
     if (nJugades <= 0) {
@@ -166,37 +230,44 @@ function comprovacions() {
         document.getElementById("totalPartidas").textContent = totalPartidas
         document.getElementById("partidesGuanyades").textContent = partidasGuanyades
         //Comprovem la maxima puntuació i formatejem el text amb el Dia Mes i Punts Mes grans
-        if (puntsPactual > maximPunts) {
-            maximPunts = puntsPactual
+        //Player 1
+        if (puntsPactual[0] > maximPunts) {
+            maximPunts = puntsPactual[0]
         }
         let dataActual = new Date()
         document.getElementById("partidaMesAlta").textContent = "Dia " + dataActual.getDate() + " Mes " + (dataActual.getMonth() + 1) + " Punts " + maximPunts
+        //Player 2
+        if (puntsPactual[1] > maximPunts) {
+            maximPunts = puntsPactual[1]
+        }
+        document.getElementById("partidaMesAlta2").textContent = "Dia " + dataActual.getDate() + " Mes " + (dataActual.getMonth() + 1) + " Punts " + maximPunts
         //Desacivem les lletres
         desactivarLletres()
+        alert("Cap dels 2 jugadors ha guanyat")
     }
 }
 //Funcio per a desactivar totes les leltres
-function desactivarLletres(){
-    for(i=1;i<=26;i++){
-        document.getElementById("lletra"+i).disabled=true;
-        document.getElementById("lletra"+i).style.color="red";
-        document.getElementById("lletra"+i).style.borderColor="red";
-        document.getElementById("lletra"+i).style.backgroundColor="yellow";
+function desactivarLletres() {
+    for (i = 1; i <= 26; i++) {
+        document.getElementById("lletra" + i).disabled = true;
+        document.getElementById("lletra" + i).style.color = "red";
+        document.getElementById("lletra" + i).style.borderColor = "red";
+        document.getElementById("lletra" + i).style.backgroundColor = "yellow";
     }
 }
 //Funcio per a activar totes les lletres
-function activarLletres(){
-    for(i=1;i<=26;i++){
-        document.getElementById("lletra"+i).disabled=false;
-        document.getElementById("lletra"+i).style.color="black";
-        document.getElementById("lletra"+i).style.borderColor="grey";
-        document.getElementById("lletra"+i).style.backgroundColor="yellow";
+function activarLletres() {
+    for (i = 1; i <= 26; i++) {
+        document.getElementById("lletra" + i).disabled = false;
+        document.getElementById("lletra" + i).style.color = "black";
+        document.getElementById("lletra" + i).style.borderColor = "grey";
+        document.getElementById("lletra" + i).style.backgroundColor = "yellow";
     }
 }
 //Funcio per a desactivar una sola lletra donada el seu ID
-function desactivarLletra(lletra){
-    document.getElementById(lletra).disabled=true;
-    document.getElementById(lletra).style.color="red";
-    document.getElementById(lletra).style.borderColor="red";
-    document.getElementById(lletra).style.backgroundColor="yellow";
+function desactivarLletra(lletra) {
+    document.getElementById(lletra).disabled = true;
+    document.getElementById(lletra).style.color = "red";
+    document.getElementById(lletra).style.borderColor = "red";
+    document.getElementById(lletra).style.backgroundColor = "yellow";
 }
